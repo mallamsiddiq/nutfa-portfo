8cb3486222bef4e313bcf531cbf8dcbb019f874f
akinyemi sodiq <sidiq@devcenter.africa>
2023-02-21 02:05:11 +0100

    dealing with static files

--- nutfa/settings.py
+++ nutfa/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "frontend/build/static"),
+    os.path.join(BASE_DIR, "frontend/build/static"),
+    os.path.join(BASE_DIR, "frontend/build"),
 ]
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
